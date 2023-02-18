df21b34cbfbc0bb8902a08ad9b85624ea9c0842c
MixMax00 <mmix00595@gmail.com>
2023-02-19 01:04:12 +0600

    sidebar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,24 @@
 $(document).ready(function(){
     $("#toggleBar").click(function(){
         $("#sidebar").toggleClass("hide");
-        $(".main").toggleClass("show");
-        $(".sidebar").toggleClass("hideMobile");
+        $(".main-content").toggleClass("show");
+        $(".sidebar").removeClass('sidebar-hide');
+    });
+
+
+    $(".sub-menu ul").hide();
+    $(".sub-menu a").click(function(){
+        $(this).parent(".sub-menu").children("ul").slideToggle("100");
+        $(this).find(".right").toggleClass("fa fa-angle-up fa fa-angle-down");
+    });
+
+
+
+    $(".profile").hide();
+
+    $("#profile").click(function(){
+        $(".profile").toggle();
     });
 });
+
 
