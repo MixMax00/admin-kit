07327497339b74f9ad938be3e68014ce743ff383
PC <mmix00595@gmail.com>
2023-02-25 22:59:31 +0630

    table and form

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,11 @@ $(document).ready(function(){
   })()
 
 
+
+
+
+window.$ = window.jquery = require('./node_modules/jquery');
+window.dt = require('./node_modules/datatables.net')();
+window.$('#table_id').DataTable();
+
+
